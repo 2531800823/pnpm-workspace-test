b639ac763412b76d7429ed03c86284facf5d90ba
2531800823 <2531800823@qq.com>
2022-09-04 20:25:22 +0800

    test

--- packages/core/index.js
+++ packages/core/index.js
@@ -2,6 +2,6 @@ const sum = require("@pnpm-publish/utils");
 
 const a = {};
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 3, 2, 2];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 3, 2, 2, 1];
 
 console.log(sum(1, 2));
